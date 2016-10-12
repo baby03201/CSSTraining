fb2ec878df3e9f0a1539813cd3bb413b1dd3d7a9
Hui Qin <baby03201@gmail.com>
2016-10-12 16:06:28 +0000

    page content and footer finally added

--- CSSPractice-2/js/script.js
+++ CSSPractice-2/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   $("#slider").easySlider();
   
-  $('#tabs')
+  $('#tabs div').hide();
+  $('#tabs div:first').show();
+  $('#tabs ul li:first').addClass('active');
+  $('#tabs ul li a').click(function(){
+    $('#tabs ul li').removeClass('active');
+    $(this).parent().addClass('active');
+    $('#tabs div').hide();
+    var currentTab = $(this).attr('href');
+    $(currentTab).show();
+    return false;
+  });
 });
